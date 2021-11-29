cd02e19cbc21a9d94a43b179116a17b857f36ab9
Szabó Miklós <miki55512@gmail.com>
2021-11-29 19:51:12 +0100

    test környezet

---
+++ src/Cool.Test/CaffTests.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cool.Test
+{
+    [TestClass]
+    public class CaffTests
+    {
+        private readonly 
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
