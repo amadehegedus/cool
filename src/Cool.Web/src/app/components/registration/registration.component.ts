fee1953b8132f833f3032ca16200a5f4bcc5ffe1
Amadé Hegedüs <amade.hegedus@gmail.com>
2021-11-30 18:48:14 +0100

    Registration

--- src/Cool.Web/src/app/components/registration/registration.component.ts
+++ src/Cool.Web/src/app/components/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService, IRegisterDto, RegisterDto } from 'src/app/api/app.generated';
+declare var bootstrap: any;
+import * as shajs from 'sha.js';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +9,86 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  public emailAlert: boolean = false;
+  public usernameAlert: boolean = false;
+  public passwordAlert: boolean = false;
+  public emptyInputAlert: boolean = false;
+  public shortPasswordAlert: boolean = false;
+  public serverAlert: boolean = false;
 
-  constructor() { }
+  public formModel: {
+    name: string;
+    email: string;
+    username: string;
+    password: string;
+    repassword: string;
+  } = { name: "", email: "", username: "", password: "", repassword: "" };
 
-  ngOnInit(): void {
+  constructor(private accountService: AccountService) {
   }
 
+  ngOnInit(): void {
+    this.resetAlerts();
+  }
+
+  private resetAlerts() {
+    this.emailAlert = false;
+    this.usernameAlert = false;
+    this.passwordAlert = false;
+    this.emptyInputAlert = false;
+    this.shortPasswordAlert = false;
+    this.serverAlert = false;
+  }
+
+  private validateFormModel(): boolean {
+    this.resetAlerts();
+    if (this.formModel.name == "" ||
+      this.formModel.email == "" ||
+      this.formModel.username == "" ||
+      this.formModel.password == "" ||
+      this.formModel.repassword == "") {
+      this.emptyInputAlert = true;
+      return false;
+    }
+    if (this.formModel.password != this.formModel.repassword) {
+      this.passwordAlert = true;
+      return false;
+    }
+    if (this.formModel.password.length < 8) {
+      this.shortPasswordAlert = true;
+      return false;
+    }
+    return true;
+  }
+
+  public onClickRegistration() {
+    if (!this.validateFormModel())
+      return;
+    this.registration();
+  }
+
+  private generateRandomSalt(): string {
+    return Math.random().toString(36).substring(2);
+  }
+
+  private getRegisterDto() {
+    const salt: string = this.generateRandomSalt();
+    const saltedPassword: string = salt + this.formModel.password;
+    const hashedSaltedPassword: string = shajs('sha256').update(saltedPassword).digest('hex');
+
+    const iRegisterDto: IRegisterDto = {
+      userName: this.formModel.username,
+      passwordHash: hashedSaltedPassword,
+      salt: salt,
+      fullName: this.formModel.name,
+      email: this.formModel.email,
+    };
+    return new RegisterDto(iRegisterDto);
+  }
+
+  private registration() {
+    this.accountService.register(this.getRegisterDto()).subscribe(result => {
+      console.log(result);
+    })
+  }
 }
