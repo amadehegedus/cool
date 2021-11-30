a2e2fd6e8d6640a782e57305f5f8fd3e14949c19
Gyulai Levente <gy.levi0@gmail.com>
2021-11-30 16:11:35 +0100

    Resolve navigation on menu

--- src/Cool.Web/src/app/app-routing.module.ts
+++ src/Cool.Web/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {BrowserComponent} from "./components/browser/browser.component";
+import {UploaderComponent} from "./components/uploader/uploader.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'browser',
+    component: BrowserComponent,
+    //canActivate: [RoleGuardService],
+  },
+  {
+    path: 'upload',
+    component: UploaderComponent,
+    //canActivate: [RoleGuardService],
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    //canActivate: [RoleGuardService],
+  },
+  {
+    path: '**',
+    redirectTo: 'browser',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
