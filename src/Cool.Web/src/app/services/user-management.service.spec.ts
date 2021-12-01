c81461ef051eaf69e5495a584f73c32c239e9c17
Gyulai Levente <gy.levi0@gmail.com>
2021-12-01 16:33:20 +0100

    update deps

--- src/Cool.Web/src/app/services/user-management.service.spec.ts
+++ src/Cool.Web/src/app/services/user-management.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TokenDecoderService } from './token-decoder.service';
+import {UserManagementService} from "./user-management.service";
+
+import 'jasmine';
+
 
 describe('TokenDecoderService', () => {
-  let service: TokenDecoderService;
+  let localStore: any;
+  let service: UserManagementService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TokenDecoderService);
+    service = TestBed.inject(UserManagementService);
+    localStore = {};
+
+    spyOn(localStorage, 'getItem').and.callFake((key) =>
+      key in localStore ? localStore[key] : null
+    );
+    spyOn(localStorage, 'setItem').and.callFake(
+      (key, value) => (localStore[key] = value + '')
+    );
+    spyOn(localStorage, 'clear').and.callFake(() => (localStore = {}));
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('', () => {
+    const result = service.getPayload();
+    expect(result).toBe('');
   });
+
 });
